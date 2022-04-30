85f52611256add636ef01a52cc837101e76937d5
Arul prasad frontend <arul.prasad@doodleblue.com>
2022-04-30 19:10:27 +0530

    Increment decrement

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Hello world" /> -->
+  <IncremenDecrement title="Increment And Decrement" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import IncremenDecrement from "./components/IncrementDecrement.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    IncremenDecrement,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/IncrementDecrement.vue
@@ -0,0 +1,47 @@
+<template>
+  <h1 class="title">{{ title }}</h1>
+
+  <h3 class="value">{{ count }}</h3>
+  <div class="button-block">
+    <button class="button" @click="increment">Increment</button>
+    <button class="button" @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IncremenDecrement",
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count > 0 && this.count--;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.title {
+  color: black;
+  font-size: 24px;
+  font-weight: 600;
+}
+
+.value {
+  font-size: 46px;
+}
+
+.button {
+  margin: 0px 20px;
+}
+</style>
